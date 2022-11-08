1ae617417e4fbd13af96fb2ebfd3127d9d052a3f
Marek Vcelak <vcelak.marek@gmail.com>
2022-11-08 01:23:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import ReactDOM from "react-dom/client";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
+  return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet animal="Dog" name="Luna" breed="Havanese" />
-      <Pet animal="Bird" name="Pepper" breed="Cockatiel" />
-      <Pet animal="Cat" name="Doink" breed="Mixed" />
+      <SearchParams />
     </div>
+  );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);

5f858ae3e6dd5c4b958eb625f771f1d72b913850
Marek Vcelak <vcelak.marek@gmail.com>
2022-11-11 01:19:09 +0100

    Update Results.jsx

--- src/Results.jsx
+++ src/Results.jsx
@@ -3,7 +3,7 @@ import Pet from "./Pet";
 const Results = ({ pets }) => {
     return (
         <div className="search">
-            {!pets.length ? (
+            {!pets.length ? (               //ternary operator    condition ? ifTrue : ifFalse
                 <h1>No Pets Found</h1>
             ) : (
                 pets.map(pet => (
